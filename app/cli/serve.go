9d7d8db97aeb060176e0f9402f0b0f93204ebe7b
Ivan Mirić <1009277+imiric@users.noreply.github.com>
2024-04-06 11:01:35 +0200

    feat(cli): gracefully shutdown the web server

--- app/cli/serve.go
+++ app/cli/serve.go
@@ -1,6 +1,15 @@
 package cli
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
 	actx "go.hackfix.me/disco/app/context"
 	"go.hackfix.me/disco/web/server"
 )
@@ -16,7 +25,35 @@ func (s *Serve) Run(appCtx *actx.Context) error {
 	if err != nil {
 		return err
 	}
-	// TODO: Handle graceful shutdown.
+
+	// Gracefully shutdown the server if a process signal is received, or the
+	// main context is done.
 	// See https://dev.to/mokiat/proper-http-shutdown-in-go-3fji
-	return srv.ListenAndServe()
+	srvDone := make(chan error)
+	go func() {
+		srvErr := srv.ListenAndServe()
+		slog.Debug("web server shutdown")
+		srvDone <- srvErr
+	}()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case s := <-sigCh:
+		slog.Debug("process received signal", "signal", s)
+	case <-appCtx.Ctx.Done():
+		slog.Debug("app context is done")
+	case srvErr := <-srvDone:
+		if srvErr != nil && !errors.Is(srvErr, http.ErrServerClosed) {
+			return fmt.Errorf("web server error: %w", srvErr)
+		}
+		return nil
+	}
+
+	if err := srv.Shutdown(appCtx.Ctx); err != nil && !errors.Is(err, context.DeadlineExceeded) {
+		return fmt.Errorf("failed shutting down web server: %w", err)
+	}
+
+	return nil
 }
